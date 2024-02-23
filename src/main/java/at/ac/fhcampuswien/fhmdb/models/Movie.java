b41e6dfee9cc911c37d1716166dcb88af2387e99
Jochen Huber <jochen.huber@stud.fh-campuswien.ac.at>
2024-02-23 13:21:00 +0100

    -added Genres to MovieCell; -added text-whiteItalic to styles.css; -personalised home-view.fxml; -added more dummy movies to cover all possible genres

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -25,8 +25,19 @@ public class Movie {
         return description;
     }
 
+    public List<Genre> getGenres() {
+        return genres;
+    }
+
     public static List<Movie> initializeMovies(){
         List<Movie> movies = new ArrayList<>();
+        movies.add(new Movie("Star Trek II: The Wrath of Khan", "With the assistance of the Enterprise crew, Admiral Kirk must stop an old nemesis, Khan Noonien Singh, from using the life-generating Genesis Device as the ultimate weapon.", List.of(Genre.SCIENCE_FICTION, Genre.ADVENTURE, Genre.ACTION)));
+        movies.add(new Movie("The Lord of the Rings: The Return of the King", "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.", List.of(Genre.FANTASY, Genre.ADVENTURE)));
+        movies.add(new Movie("Les Misérables", "In 19th-century France, Jean Valjean, who for decades has been hunted by the ruthless policeman Javert after breaking parole, agrees to care for a factory worker's daughter. The decision changes their lives forever.", List.of(Genre.MUSICAL, Genre.HISTORY)));
+        movies.add(new Movie("The Hateful Eight", "In the dead of a Wyoming winter, a bounty hunter and his prisoner find shelter in a cabin currently inhabited by a collection of nefarious characters.", List.of(Genre.WESTERN, Genre.THRILLER)));
+        movies.add(new Movie("Starship Troopers", "Humans in a fascist, militaristic future wage war with giant alien bugs.", List.of(Genre.SCIENCE_FICTION, Genre.WAR, Genre.ACTION, Genre.HORROR)));
+        movies.add(new Movie("The Shining", "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.", List.of(Genre.HORROR, Genre.THRILLER)));
+        movies.add(new Movie("Invictus", "Nelson Mandela, in his first term as President of South Africa, initiates a unique venture to unite the Apartheid-torn land: enlist the national rugby team on a mission to win the 1995 Rugby World Cup.", List.of(Genre.DOCUMENTARY, Genre.HISTORY, Genre.SPORT)));
         movies.add(new Movie("Life Is Beautiful", "When an open-minded Jewish waiter and his son become victims of the Holocaust, he uses a perfect mixture of will, humor and imagination to protect his son from the dangers around their camp.", List.of(Genre.DRAMA, Genre.ROMANCE)));
         movies.add(new Movie("The Usual Suspects", "The sole survivor of a pier shoot-out tells the story of how a notorious criminal influenced the events that began with five criminals meeting in a seemingly random police lineup.", List.of(Genre.CRIME, Genre.DRAMA, Genre.MYSTERY)));
         movies.add(new Movie("Puss in Boots", "An outlaw cat, his childhood egg-friend, and a seductive thief kitty set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor, and regain the trust of his mother and town.", List.of(Genre.COMEDY, Genre.FAMILY, Genre.ANIMATION)));
--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -2,6 +2,7 @@ package at.ac.fhcampuswien.fhmdb.ui;
 
 import at.ac.fhcampuswien.fhmdb.models.Movie;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Background;
@@ -12,7 +13,8 @@ import javafx.scene.paint.Color;
 public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
-    private final VBox layout = new VBox(title, detail);
+    private final Label genre = new Label();
+    private final VBox layout = new VBox(title, detail, genre);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -28,11 +30,16 @@ public class MovieCell extends ListCell<Movie> {
                             ? movie.getDescription()
                             : "No description available"
             );
-
+            genre.setText(
+                    movie.getGenres() != null
+                            ? movie.getGenres().toString().replace("[", "").replace("]", "")
+                            : "No genres available"
+            );
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
+            genre.getStyleClass().add("text-whiteItalic");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
@@ -41,7 +48,7 @@ public class MovieCell extends ListCell<Movie> {
             detail.setWrapText(true);
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
-            layout.alignmentProperty().set(javafx.geometry.Pos.CENTER_LEFT);
+            layout.alignmentProperty().set(Pos.CENTER_LEFT);
             setGraphic(layout);
         }
     }
